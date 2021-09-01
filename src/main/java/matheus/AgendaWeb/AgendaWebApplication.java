bc9606194c49211de2c79cf07ce0d8d1279d10b7
matheus.borges <matheus.borges@engemail.com.br>
2021-08-31 21:05:40 -0300

    Primeiro commit

---
+++ src/main/java/matheus/AgendaWeb/AgendaWebApplication.java
@@ -0,0 +1,13 @@
+package matheus.AgendaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/matheus/AgendaWeb/Controllers/AgendaController.java
@@ -0,0 +1,14 @@
+package matheus.AgendaWeb.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AgendaController {
+    
+    @RequestMapping("/ola")
+    public String exibirMensagem() {
+        return "Ola mundo";
+    }
+}
